6e8266d6665766dfd5ea0332662cab047e3f4eeb
Fosrey <fosrey@gmail.com>
2021-06-10 22:16:50 -0500

    Empaquetado

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,26 +1,15 @@
 
-import { ModuleWithProviders } from '@angular/core';
-import {Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './components/sections/home/home.component';
-import { ServiciosocialComponent } from './components/sections/admisiontramites/serviciosocial/serviciosocial.component';
-import { TitulacionComponent } from './components/sections/admisiontramites/titulacion/titulacion.component';
-import { BecasComponent } from './components/sections/admisionytramites/becas/becas.component';
-import { AdmisiontramitesComponent } from './components/sections/admisiontramites/admisiontramites.component';
-
-// Rutas
-const appRoutes: Routes = [
-    { path: '', component: HomeComponent },
-    {path: 'admisiontramites',component: AdmisiontramitesComponent},
-    {path: 'serviciosocial',component: ServiciosocialComponent },
-    {path: 'titulacion',component: TitulacionComponent}
-  
 import { ModuleWithProviders, Component } from '@angular/core';
 import {Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/sections/home/home.component';
-import { InscripcionComponent } from './components/sections/admisionytramites/inscripcion/inscripcion.component';
+import { InscripcionComponent } from './components/sections/admisiontramites/inscripcion/inscripcion.component';
 import { ServiciosocialComponent } from './components/sections/admisiontramites/serviciosocial/serviciosocial.component';
 import { TitulacionComponent } from './components/sections/admisiontramites/titulacion/titulacion.component';
 import { AdmisiontramitesComponent } from './components/sections/admisiontramites/admisiontramites.component';
+
+import { BecasComponent } from './components/sections/admisiontramites/becas/becas.component';
+
+
 // Rutas
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
--- src/app/components/sections/sections.module.ts
+++ src/app/components/sections/sections.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { MaterialModule } from '../material/material.module';
-import { BecasComponent } from './admisionytramites/becas/becas.component';
+import { BecasComponent } from './admisiontramites/becas/becas.component';
 
 import { ServiciosocialComponent } from './admisiontramites/serviciosocial/serviciosocial.component';
 import { TitulacionComponent } from './admisiontramites/titulacion/titulacion.component';
 import { AdmisiontramitesComponent } from './admisiontramites/admisiontramites.component';
 
-import { InscripcionComponent } from './admisionytramites/inscripcion/inscripcion.component';
+import { InscripcionComponent } from './admisiontramites/inscripcion/inscripcion.component';
 
 
 @NgModule({
