89ea1d6928f18f88d8b9e759fd01864fbfdd19e3
EDUARDO <gtainside222@gmail.com>
2021-09-15 16:02:36 -0500

    MODULOS 0 Y 100

--- NMEA.py
+++ NMEA.py
@@ -1,97 +1,140 @@
+
+
+
 from typing import List
-# ------ Menu
+import csv
 
-#Variable menu principal sirve para elegir una opcion, dos opciones, seguir el proceso o terminarlo, si se introduce cualquier otra cosa diferente dira entrada invalida.
+RUN = False
+LONGD = 12
+ALERT = []
+
+RUN = input("ENCEDER O APAGAR ON / OFF")
+
+config = open('config.txt','r')
+
+LMAC = config.readline()
+RMCE = config.readline()
+IACT = config.readline()
+
+config.close()
+
+LOMAC = list(LMAC.split(','))
+
+print(LOMAC)
 
 
-
-
-
-
-# VARIABLES
-# LMAC = LISTA DE DISPOSITIVOS,  RMC = VARIABLE ESTADO ON OFF, PACT = PERIODO DE ACTUALIZACION, LOMAC ES LISTA DE DISPOSITIVOS
-
-##
-
-while 1>0:
+##while RUN in "ON":
     
-        print("Iniciando proceso...")
-        ALERT = []
-
-###----------ESTADO 0-----------
-        disp = str(input("NOMBRE DE DISPOSITIVO \n"))
-
-        LMAC = ""
-        RMC =False
-        PACT = 0
-        TF = 12
-
-        #Variables GGA
+    
+class Estat0:
         
+    DISP = False
+             
+    NDISPBL = ""
+    DDISPBL = ""
+            
+BLUET = Estat0()
+
+print("ESTADO CERO")
+
+while BLUET.DISP == False:
         
-        #--------------
-
-        config = open('config.txt','r')
-
-        LMAC = config.readline()
-        RMC = config.readline()
-        PACT = config.readline()
-
-        config.close()
-        LOMAC = list(LMAC.split(','))
-
-
-
-        if len(disp) == TF:
-
-            if disp in LOMAC:
-
-                print("EL DISPOSITIVO", disp, "EXISTE EN LA CONFIGURACION")
-
-                print("... Recibiendo datos (GGA)")
-
-
-            else:
-                ALERT.append(" E 0 DN ")
-                print("EL DISPOSITIVO", disp, "NO EXISTE EN LA CONFIGURACION")
-
-               
-
+    BLUET.NDISPBL = str(input("Nombre del dispositivo"))
+    BLUET.DDISPBL = str(input("Direccion MAC"))
+        
+    if len(BLUET.DDISPBL) == LONGD:
+        
+        if BLUET.DDISPBL in LOMAC:
+            BLUET.DISP = True
+            print("El dispositivo existe en la configuracion")
         else:
-
-            print("LA DIRECCION MAC NO VALIDA")
-
-
-
-##-----------TERMINACION DE ESTADO 0------------
-
-#-----------------ESTADO 100--------------------
-
+            ALERT.append("E 0 DN")
+            print("El dispositivo no existe en la configuracion")
+    else:
+        ALERT.append("E 0 DM")
+        print("Error en la MAC")
+   
+print(ALERT)   
 
 
 
+class Estat100:
+    
+    
+    ESTATGGA = False
+    LGGA = []
+    IDGGA = ""
+    UTC = ""
+    LAT = ""
+    DIRLAT = ""
+    LONGI = ""
+    DIRLONGI = ""
+    FIX = 0
+    SAT = 0
+    HDOP = ""
+    ALT = 0
+    ALTORT = 0
+    ALTUM = 0
+    SEPGEO = ""
+    SEPGEOM = ""
+    ANTREGD = ""
+    REFESTID = ""
+
+GGA = Estat100()
+
+print("ESTADO 100")
+
+if BLUET.DISP == True:
+    
+    while GGA.ESTATGGA == False :
+        
+        gga = open('GGA.txt','r')
+
+        GGA.LGGA = list(gga.read().split(','))
+        
+        GGA.IDGGA = GGA.LGGA[0]
+        GGA.UTC = GGA.LGGA[1]
+        GGA.LAT = GGA.LGGA[2]
+        GGA.DIRLAT = GGA.LGGA[3]
+        GGA.LONGI = GGA.LGGA[4]
+        GGA.DIRLONGI = GGA.LGGA[5]
+        GGA.FIX = int(GGA.LGGA[6])
+        GGA.SAT = int(GGA.LGGA[7])
+        GGA.HDOP = GGA.LGGA[8]
+        GGA.ALT = GGA.LGGA[9]
+        GGA.ALTORT = GGA.LGGA[10]
+        GGA.SEPGEO = GGA.LGGA[11]
+        GGA.SEPGEOM = GGA.LGGA[12]
+        GGA.ANTREGD = GGA.LGGA[13]
+        GGA.REFESTID = GGA.LGGA[14]
+        gga.close()
+        
+        if GGA.FIX > 0 :
+            
+            if GGA.SAT > 3:
+                
+                GGA.ESTATGGA = True
+                print("SATELITES MAYORES A 3","CON UNA SENAL FIX DE:",GGA.FIX)
+            else:
+                GGA.ESTATGGA = True
+                ALERT.append("E 100 SAT")
+                print("sin señal")
+        else:
+               
+            print("FIX NO VALIDO")
+    else:
+        
+        print("ELEMENTOS GUARDADOS",GGA.LGGA)
+else:
+    
+    print("TU QUE HACES AQUI?")        
+    
+class Estat110:
+    
+    
+    
+    
+##else:
+   ## print("DISPOSITIVO APAGADO")
 
 
-
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Finalizacion de menu, opcion de cerrado y comparacion de opciones
